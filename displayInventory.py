b260280506894e7bba1a988f22af539ea826d8b7
Brett McElvain <brett@Bretts-MacBook-Pro.local>
2022-06-03 15:36:01 -0500

    Added a colon to Inventory statement

--- displayInventory.py
+++ displayInventory.py
@@ -1,7 +1,7 @@
 stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'ring': 1, 'apple': 12}
 
 def displayInventory(inventory):
-    print('Inventory')
+    print('Inventory:')
     total_items = 0
     for key in stuff:
         print(stuff[key], key)
