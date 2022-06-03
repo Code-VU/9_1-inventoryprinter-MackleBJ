9a3c5a18baf3b28e9a565fcb7830e139deee311c
Brett McElvain <brett@Bretts-MacBook-Pro.local>
2022-06-03 16:03:59 -0500

    Changed variable name within the function.

--- displayInventory.py
+++ displayInventory.py
@@ -3,9 +3,9 @@ stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'ring': 1, 'apple': 12}
 def displayInventory(inventory):
     print('Inventory:')
     total_items = 0
-    for key in stuff:
-        print(stuff[key], key)
-        total_items += stuff[key]
+    for key in inventory:
+        print(inventory[key], key)
+        total_items += inventory[key]
     
     print('Total number of items: {}'.format(total_items))
 
